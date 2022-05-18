5b0c4df0edca328f6ea7b895ee567e944f887d1f
Tamashhi <100945549+Tamashhi@users.noreply.github.com>
2022-05-18 10:02:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -260,8 +260,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 2) {
-      newMintAmount = 2;
+    if (newMintAmount > 1000) {
+      newMintAmount = 1000;
     }
     setMintAmount(newMintAmount);
   };
