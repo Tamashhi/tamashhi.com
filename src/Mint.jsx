8f486457b8d3b284ae2e9041ef5f67ecb47e1cd7
Tamashhi <100945549+Tamashhi@users.noreply.github.com>
2022-07-18 12:27:48 +0100

    Update Mint.jsx

--- src/Mint.jsx
+++ src/Mint.jsx
@@ -92,7 +92,7 @@ export const StyledImg = styled.img`
   border: 4px  var(--secondary);
   background-color: var(--accent);
   border-radius: 100%;
-  width: 0px;
+  width: 200px;
   @media (min-width: 900px) {
     width: 250px;
   }
